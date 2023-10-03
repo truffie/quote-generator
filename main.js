2c0115614925c5f145566023f21262ba9f89e186
truffie <trufanov.vl2013@gmail.com>
2023-10-03 14:31:49 +0300

    create animation

---
+++ main.js
@@ -0,0 +1,28 @@
+let quote = document.querySelector('.random-quotes');
+let qoutesList = ['If you fall asleep now, you will dream. If you study now, you will live your dream.',
+  'When you think it’s too late, the truth is, it’s still early.',
+  'The pain of studying is only temporary. But the pain of not knowing — ignorance — is forever.',
+  'Studying is not about time. It’s about effort.',
+  'Life is not all about studying. But if you can’t even conquer this little part of life, then what else can you possibly do?',
+  'Success only comes with self-management and determination.',
+  'If you don’t walk today, you’ll have to run tomorrow.',
+  'The level of education is in direct correlation with your salary.',
+  'When today is over, it will never come back.',
+  'Even now, your enemies are eagerly flipping through books.']
+
+generateQuotes = (list) => {
+  let random = Math.floor(Math.random() * list.length);
+  quote.textContent = list[random];
+}
+setInterval(generateQuotes, 2000, qoutesList)
+
+
+const sum = (arr) => {
+
+    if(typeof arr !== 'object' || arr.every(el => el > 0)) {
+      return 'error'
+    }
+    return arr.reduce((acc, el) => acc * el, 1);
+
+}
+console.log(sum([-1, 2, 3, 4, 5]));
